76a35bed6f0ee85a4f79b9cb887e8501c692f3ce
ManahariDahal <manahari.dahal1@gmail.com>
2021-02-20 00:48:32 -0700

    Gallery page done

--- lesson7/js/caption.js
+++ lesson7/js/caption.js
@@ -1,5 +1,13 @@
 let caption = document.querySelectorAll(".caption");
 
 for (let i = 0; i < caption.length; i++) {
-    caption[i].textContent = "This is a caption";
+    caption[0].textContent = "This is a caption";
+    caption[1].textContent = "Habata Itara Modoranai to itte";
+    caption[2].textContent = "Mezashita no wa aoi aoi ano sora";
+    caption[3].textContent = "You talking to me?";
+    caption[4].textContent = "Hello there!";
+    caption[5].textContent = "GENERAL KENOBI!";
+    caption[6].textContent = "GET TO THE CHOPPA!";
+    caption[7].textContent = "It's LeviOsa, not LevioSAAAA";
+    caption[8].textContent = "HARRY POTTA";
 }
