7ee4bfbc7fcbb675b911eccfe5d9f7fcf67fd8b2
ManahariDahal <manahari.dahal1@gmail.com>
2021-03-05 21:31:24 -0700

    Home page done

--- 1-main/js/main.js
+++ 1-main/js/main.js
@@ -5,8 +5,6 @@ window.addEventListener('load', () => {
     ham.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')},false);
 
     window.onresize = () => {if(window.innerWidth >760) mainnav.classList.remove('responsive');};
-
-
     
     let date = new Date("01/29/2021");
     document.getElementById('date').innerHTML = date.toDateString();
